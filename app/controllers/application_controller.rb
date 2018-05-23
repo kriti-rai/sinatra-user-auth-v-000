46258da444ac5329abaabe69fd450eb93d3e8847
Kriti Rai <kriti.rai@gmail.com>
2018-05-23 23:32:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,6 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    # binding.pry
     session[:id] = @user.id
 
     redirect '/users/home'
