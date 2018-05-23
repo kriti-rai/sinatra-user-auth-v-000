e7df6ebc0095d6b2b52cb7cb66739b04f71d7f1d
Kriti Rai <kriti.rai@gmail.com>
2018-05-23 23:30:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    @user = User.find(session[:id])
+    @user = User.find(session[:id].to_i)
     erb :'/users/home'
   end
 
